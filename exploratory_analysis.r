772c6535982a4585247b746f6901b760d41caba5
heydenlucas <lucas.heyden@icloud.com>
2025-05-20 11:54:00 +0000

    Adding export files to check and making analysis with the data

--- exploratory_analysis.r
+++ exploratory_analysis.r
@@ -1,25 +1,50 @@
-library(readr)
 library(tidyverse)
+library(lubridate)
+library(ggplot2)
+library(ggrepel)
+library(readr)
+library(dplyr)
+library(tidyr)
 library(here)
-library(janitor)
 library(skimr)
+library(janitor)
+library(corrplot)
+library(ggcorrplot)
+library(cluster)
+library(factoextra)
+
 df_daily_activity <- read_csv("/cloud/project/raw_data_bellabeat/dailyActivity_merged.csv")
 df_daily_calories <- read_csv("/cloud/project/raw_data_bellabeat/dailyCalories_merged.csv")
 df_daily_intensities <- read_csv("/cloud/project/raw_data_bellabeat/dailyIntensities_merged.csv")
 df_daily_steps <- read_csv("/cloud/project/raw_data_bellabeat/dailySteps_merged.csv")
-df_heartrate_seconds <- read_csv("/cloud/project/raw_data_bellabeat/heartrate_seconds_merged.csv")
+df_daily_sleep <- read_csv("/cloud/project/raw_data_bellabeat/sleepDay_merged.csv")
 df_hourly_calories <- read_csv("/cloud/project/raw_data_bellabeat/hourlyCalories_merged.csv")
-df_hourly_intensities <- read_csv("/cloud/project/raw_data_bellabeat/hourlyIntensities_merged.csv")
-df_hourly_steps <- read_csv("/cloud/project/raw_data_bellabeat/hourlySteps_merged.csv")
-df_minute_calories_narrow <- read_csv("/cloud/project/raw_data_bellabeat/minuteCaloriesNarrow_merged.csv")
-df_minute_calories_wide <- read_csv("/cloud/project/raw_data_bellabeat/minuteCaloriesWide_merged.csv")
-df_minute_intensities_narrow <- read_csv("/cloud/project/raw_data_bellabeat/minuteIntensitiesNarrow_merged.csv")
-df_minute_intensities_wide <- read_csv("/cloud/project/raw_data_bellabeat/minuteIntensitiesWide_merged.csv")
-df_minute_METs_narrow <- read_csv("/cloud/project/raw_data_bellabeat/minuteMETsNarrow_merged.csv")
-df_minute_sleep <- read_csv("/cloud/project/raw_data_bellabeat/minuteSleep_merged.csv")
-df_minute_steps_narrow <- read_csv("/cloud/project/raw_data_bellabeat/minuteStepsNarrow_merged.csv")
-df_minute_steps_wide <- read_csv("/cloud/project/raw_data_bellabeat/minuteStepsWide_merged.csv")
-df_sleep_day <- read_csv("/cloud/project/raw_data_bellabeat/sleepDay_merged.csv")
-df_weight_log_info <- read_csv("/cloud/project/raw_data_bellabeat/weightLogInfo_merged.csv")
 
-glimpse(df_daily_activity)
+
+# Calculate the percentage of duplicated and missing values
+dp_hourly_calories <- sum(duplicated(df_hourly_calories)) * 100 / nrow(df_hourly_calories)
+na_hourly_calories <- sum(is.na(df_hourly_calories)) * 100 / nrow(df_hourly_calories)
+
+dp_hourly_calories
+na_hourly_calories
+
+str(df_hourly_calories)
+
+# Separa em coluna de data (apenas a data) e coluna de hora (apenas a hora 0-23)
+df_hourly_calories$activity_datetime <- as.POSIXct(
+  df_hourly_calories$ActivityHour,
+  format = "%m/%d/%Y %I:%M:%S %p"
+)
+
+# Usa lubridate::hour() para obter a hora de 0 a 23
+df_hourly_calories$hour <- hour(df_hourly_calories$activity_datetime)
+
+
+df_hourly_calories <- df_hourly_calories %>%
+  arrange(desc(hour))
+head(df_hourly_calories,100)
+
+
+
+
+
